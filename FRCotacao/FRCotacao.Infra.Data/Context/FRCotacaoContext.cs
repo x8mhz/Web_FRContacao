143e6aaf3f5fcc89c4ee8b65540bbdb7bbca0179
Fabricio Ramos <fabricio.ramoss@hotmail.com>
2017-09-29 11:29:37 -0300

    Base

---
+++ FRCotacao/FRCotacao.Infra.Data/Context/FRCotacaoContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FRCotacao.Infra.Data.Context
+{
+    public class FRCotacaoContext : DbContext
+    {
+        public FRCotacaoContext()
+            : base("FRCotacao")
+        {
+              
+        }
+    }
+}
---
+++ FRCotacao/FRCotacao.UI.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FRCotacao.UI.Web.MvcApplication" Language="C#" %>
