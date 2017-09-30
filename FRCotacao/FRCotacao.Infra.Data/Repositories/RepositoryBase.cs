fee57075fe1a7aadc736ce74ff1398610f4befe8
Fabricio Ramos <fabricio.ramoss@hotmail.com>
2017-09-29 22:04:12 -0300

    Base 3

---
+++ FRCotacao/FRCotacao.Infra.Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using FRCotacao.Infra.Data.Context;
+using FRCotacao.Model.Interfaces;
+
+namespace FRCotacao.Infra.Data.Repositories
+{
+    public class RepositoryBase<TEntity> : IDisposable, IRepositoryBase<TEntity> where TEntity : class
+    {
+        protected FRCotacaoContext Db = new FRCotacaoContext();
+
+        public void Add(TEntity obj)
+        {
+            Db.Set<TEntity>().Add(obj);
+            Db.SaveChanges();
+        }
+
+        public TEntity GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepositoryBase<TEntity>.Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDisposable.Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FRCotacao/FRCotacao.Model/Interfaces/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using FRCotacao.Model.Entities;
+
+namespace FRCotacao.Model.Interfaces
+{
+    public interface IUserRepository : IRepositoryBase<User>
+    {
+
+    }
+}
